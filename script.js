6d907a6363a7db1fb810a3b8e4bf73e895fa4f07
wangunderline <wangwangrian@gmail.com>
2024-02-19 10:42:34 -0300

    Add playGame function to loop the playRound() 5 times

--- script.js
+++ script.js
@@ -12,22 +12,55 @@ function getComputerChoice() {
   return random.toLowerCase();
 }
 
-const userChoice = prompt("Do it").toLowerCase();
+let userScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        alert("You win! Rock beats scissors");
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        alert("You win! Paper beats rock");
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        alert("You win! Scissors beats paper");
-    } else if (playerSelection === computerSelection) {
-        alert(`Draw! ${playerSelection} is friends with ${computerSelection}`);
-    } else if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-        alert("You must make a valid choice.")
-    } else {
-        alert(`You lose! ${computerSelection} beats ${playerSelection}`);
-    }
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    alert("You won! Rock beats scissors");
+    userScore++;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    alert("You won! Paper beats rock");
+    userScore++;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    alert("You won! Scissors beats paper");
+    userScore++;
+  } else if (playerSelection === computerSelection) {
+    alert(`Draw! ${playerSelection} is friends with ${computerSelection}`);
+  } else if (
+    playerSelection !== "rock" &&
+    playerSelection !== "paper" &&
+    playerSelection !== "scissors"
+  ) {
+    alert("You must make a valid choice.");
+  } else {
+    alert(`You lost! ${computerSelection} beats ${playerSelection}`);
+    computerScore++;
+  }
+
+  alert(`You ${userScore} x ${computerScore}`);
 }
 
-playRound(userChoice, getComputerChoice());
+function playGame() {
+  if (confirm("New game?")) {
+    for (let i = 0; i < 5; i++) {
+      alert(`Round ${i + 1}`);
+      playRound(prompt("Do it"), getComputerChoice());
+    }
+  }
+
+  if (userScore > computerScore) {
+    alert(`You won! Final score: You ${userScore} x ${computerScore} Computer`);
+    console.log(`You ${userScore} x ${computerScore} Computer`);
+  } else if (computerScore > userScore) {
+    alert(
+      `You lost! Final score: You ${userScore} x ${computerScore} Computer`
+    );
+    console.log(`You ${userScore} x ${computerScore} Computer`);
+  } else {
+    alert(`You ${userScore} x ${computerScore} Computer. This is a draw!`);
+    console.log(`You ${userScore} x ${computerScore} Computer`);
+  }
+}
+
+playGame();
